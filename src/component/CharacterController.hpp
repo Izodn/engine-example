5ba0697ad3486a077bee5469dfb10f77bc336596
Brandon Burton <brandon@brandonbeta.com>
2017-02-07 11:18:15 -0800

    Use more efficient methods of rotation.

--- src/component/CharacterController.hpp
+++ src/component/CharacterController.hpp
@@ -22,36 +22,31 @@ class CharacterController : public Component
 				|| Input::Pressed(Input::Key::KEY_E)
 			) {
 				double deltaRad = (90.0f * (/*PI*/4*atan(1) / 180)) * deltaTime;
-				Transform* transform = m_GameObject->GetTransform();
-				Quaternion* quat = transform->GetRotation();
-				Vector3 euler = quat->ToEulerAngles();
+				Quaternion* quat = m_GameObject->GetTransform()->GetRotation();
 
 				// Roll
 				if (Input::Pressed(Input::Key::KEY_E)) {
-					euler.SetZ(euler.GetZ() - deltaRad);
+					quat->Rotate(Vector3::Forward(), -deltaRad);
 				}
 				if (Input::Pressed(Input::Key::KEY_Q)) {
-					euler.SetZ(euler.GetZ() + deltaRad);
+					quat->Rotate(Vector3::Forward(), deltaRad);
 				}
 
 				// Pitch
 				if (Input::Pressed(Input::Key::KEY_W)) {
-					euler.SetX(euler.GetX() - deltaRad);
+					quat->Rotate(Vector3::Right(), -deltaRad);
 				}
 				if (Input::Pressed(Input::Key::KEY_S)) {
-					euler.SetX(euler.GetX() + deltaRad);
+					quat->Rotate(Vector3::Right(), deltaRad);
 				}
 
 				// Yaw
 				if (Input::Pressed(Input::Key::KEY_D)) {
-					euler.SetY(euler.GetY() - deltaRad);
+					quat->Rotate(Vector3::Up(), -deltaRad);
 				}
 				if (Input::Pressed(Input::Key::KEY_A)) {
-					euler.SetY(euler.GetY() + deltaRad);
+					quat->Rotate(Vector3::Up(), deltaRad);
 				}
-
-				// Update quaternion with new rotation
-				*quat = Quaternion::FromEulerAngles(euler);
 			}
 		};
 };
