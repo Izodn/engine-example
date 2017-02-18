369d73d26bbd1d4030157b7a76b69380031c7e22
Brandon Burton <brandon@brandonbeta.com>
2017-02-17 16:28:18 -0800

    CharacterController now using m_Transform. Got rid of LinePreset

--- src/component/CharacterController.hpp
+++ src/component/CharacterController.hpp
@@ -28,33 +28,33 @@ class CharacterController : public Component
 				|| Input::Pressed(Input::Key::KEY_E)
 			) {
 				double deltaRad = (90.0f * (/*PI*/4*atan(1) / 180)) * deltaTime;
-				Quaternion* quat = m_GameObject->GetTransform()->GetRotation();
+				Quaternion* rotation = m_Transform->GetRotation();
 
 				// Roll
 				if (Input::Pressed(Input::Key::KEY_E)) {
-					quat->Rotate(Vector3::Forward(), -deltaRad);
+					rotation->Rotate(Vector3::Forward(), -deltaRad);
 				}
 				if (Input::Pressed(Input::Key::KEY_Q)) {
-					quat->Rotate(Vector3::Forward(), deltaRad);
+					rotation->Rotate(Vector3::Forward(), deltaRad);
 				}
 
 				// Pitch
 				if (Input::Pressed(Input::Key::KEY_W)) {
-					quat->Rotate(Vector3::Right(), -deltaRad);
+					rotation->Rotate(Vector3::Right(), -deltaRad);
 				}
 				if (Input::Pressed(Input::Key::KEY_S)) {
-					quat->Rotate(Vector3::Right(), deltaRad);
+					rotation->Rotate(Vector3::Right(), deltaRad);
 				}
 
 				// Yaw
 				if (Input::Pressed(Input::Key::KEY_D)) {
-					quat->Rotate(Vector3::Up(), -deltaRad);
+					rotation->Rotate(Vector3::Up(), -deltaRad);
 				}
 				if (Input::Pressed(Input::Key::KEY_A)) {
-					quat->Rotate(Vector3::Up(), deltaRad);
+					rotation->Rotate(Vector3::Up(), deltaRad);
 				}
 			} else if (Input::Pressed(Input::Key::MOUSE_ONE)) {
-				Quaternion* quat = m_GameObject->GetTransform()->GetRotation();
+				Quaternion* rotation = m_Transform->GetRotation();
 				double mouseX = Input::GetAxis(Input::Axis::HORIZONTAL);
 				double mouseY = Input::GetAxis(Input::Axis::VERTICAL);
 
@@ -62,7 +62,7 @@ class CharacterController : public Component
 				double gridY = mouseY - (m_WindowHeight / 2);
 				Vector3 target = Vector3(gridX, -gridY, 100.0f);
 
-				*quat = Quaternion::LookAt(
+				*rotation = Quaternion::LookAt(
 					*(m_GameObject->GetTransform()->GetPosition()),
 					target
 				);
